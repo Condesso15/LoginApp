1c770a9bad71552a48c6999a75e6de66099080b1
Goncalo <goncalocondesso2003@gmail.com>
2022-01-31 23:13:31 +0000

    Ola

---
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.loginapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+     EditText password;
+     boolean passwordVisible;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        password=findViewById(R.id.password);
+
+
+        password.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public  boolean onTouch(View v, MotionEvent event){
+                final int Right=2;
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    if (event.getRawX()>=password.getRight()-password.getCompoundDrawables()[Right].getBounds().width()){
+                        int selection=password.getSelectionEnd();
+                        if (passwordVisible){
+                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_baseline_visibility_off_24,0);
+                            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            passwordVisible=false;
+                        }else{
+                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_baseline_visibility_24,0);
+                            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            passwordVisible=true;
+                        }
+                        password.setSelection(selection);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+
+
+    public void ChamarEcra2(View view) {
+
+        Intent intent =  new Intent(this,Registo.class);
+        startActivity(intent);
+    }
+}
